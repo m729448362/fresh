9d51f335c7e37b35e7972514dd1b7845a6c3b96e
Administrator <Administrator@SKY-20170718TZB>
2018-04-24 22:21:56 +0800

    html

---
+++ src/main/webapp/js/painter.js
@@ -0,0 +1,11 @@
+define(function(require) {
+	var $ = require("jquery");
+	var justep = require("$UI/system/lib/justep");
+	var allData = require("./js/loadData");
+
+	var Model = function() {
+		this.callParent();
+		this.isBack;	
+	};
+	
+})
